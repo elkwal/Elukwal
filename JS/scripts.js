5e67b056308b7231659647a927bc378c5f92c483
Elukwal <elukwal3@gmail.com>
2018-10-03 22:55:55 +0300

    final commit

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,16 +1,13 @@
-var a =parseInt(prompt("Enter the side A"));
-var b =parseInt(prompt("Enter the side B"));
-var c =parseInt(prompt("Enter the side C"));
-if  (a===b && b===c) {
-alert("This is an equilateral triange");
+var a = parseInt(prompt("Enter the side A"));
+var b = parseInt(prompt("Enter the side B"));
+var c = parseInt(prompt("Enter the side C"));
+
+if (a === b && b === c) {
+  alert("This is an equilateral triange");
+} else if (a === b || b === c || c == b) {
+  alert("This is an isosceles triange");
+} else if ((a + b) > c || (b + c) > a || (a + c) > b) {
+  alert("This is a scalene triangle");
+} else {
+  alert("This is not a triangle");
 }
-else if (a===b || b===c || c==b)
-{
-  alert("This is an isosceles triange")
-  }
-  else if( (a+b) > c || (b+c) > a || (a+c) > b){
-    alert("This is a scalene triangle");
-    }
-     else {
-     alert("This is not a triangle")
-    }
--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # Triangle tracker
+
 #### A webpage giving properties of triangles, 10th August 2018
+
 #### By **Sylvia Elukwal**
+
 ## Description
+
 The Triangle tracker  opens by the title triangle tracker. It then gives a clear image of a triangle and its labeled sides. It then gives the types and properties of angles.
+
 ## Setup/Installation Requirements
+
 To start using the project use the instructions below:
-* `git clone https://github.com/elkwal/Elukwal.git`
-* `cd css`
-* `atom .`
-### License
+
+-   `git clone https://github.com/elkwal/Elukwal.git`
+-   `cd css`
+-   `atom .`
+    ### License
 
 MIT License
 Copyright (c) 2018 Moringa School
